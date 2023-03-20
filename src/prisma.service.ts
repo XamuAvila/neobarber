1a785f986bdd3838ae75c079de8fd8528c6587e0
Samuel_Avila <samuel.avila@tekever.com>
2023-03-20 16:37:17 -0300

    Configuring docker

---
+++ src/prisma.service.ts
@@ -0,0 +1,17 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect()
+      .then(() => console.log('Connected to database successfully 🚀'))
+      .catch(() => console.error('Error connecting to database 🤒'));
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
